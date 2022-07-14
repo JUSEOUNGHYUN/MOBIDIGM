5c025a62a61f5b270e8d646e5f07cce2c1a34dae
JUSEOUNGHYUN <succeed3256@naver.com>
2022-07-14 22:11:14 +0900

    MFC_한줄씩 읽어서 출력.file

---
+++ MFC/File_openDlg.h
@@ -0,0 +1,43 @@
+
+// File_openDlg.h: 헤더 파일
+//
+
+#pragma once
+
+#include <list>
+#include <string>
+// CFileopenDlg 대화 상자
+class CFileopenDlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CFileopenDlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_FILE_OPEN_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedButton1();
+	afx_msg void OnLbnSelchangeList8();
+	CListBox m_ctrlNMEAList;
+
+	std::list <std::string> m_listNMEADataString;
+};
+
+
+
