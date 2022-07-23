07fe1130c8f7ca9fd69d933880856c3181fbca7a
JUSEOUNGHYUN <succeed3256@naver.com>
2022-07-23 17:36:46 +0900

    string -> CString

---
+++ STRING/TYPE_CONVERSION/CString/String_To.cpp
@@ -0,0 +1,16 @@
+/*
+--- CT2CA ---
+CString -> string
+
+CString m_strPathName;
+string str = string(pDlg->CT2CA(m_strPathName))
+- pDlg = 지역변수의 m_strPathName를 사용하겠다는 스레드
+
+--- CT2CT ---
+string -> CString
+
+string front_Data;
+CA2CT(front_Data.c_str());
+
+*/
+
