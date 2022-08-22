8e6abb9072d70382de06d2e9ad4dd056d8e76dd5
JUSEOUNGHYUN <succeed3256@naver.com>
2022-08-22 11:46:57 +0900

    string <-> CString

--- STRING/TYPE_CONVERSION/CString/String_To.cpp
+++ STRING/TYPE_CONVERSION/CString/String_To.cpp
@@ -2,6 +2,11 @@
 --- CT2CA ---
 CString -> string
 
+1.
+CString m_strPathName;
+std::string strPathName = std::string(CT2CA(m_strPathName));
+
+2. 
 CString m_strPathName;
 string str = string(pDlg->CT2CA(m_strPathName))
 - pDlg = 지역변수의 m_strPathName를 사용하겠다는 스레드
